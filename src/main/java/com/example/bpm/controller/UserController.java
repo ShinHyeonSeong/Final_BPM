22615b4c80b4319b6618d246c5128d5333d5e89b
ppark__jjong <101304928+Parkjonghyeok2@users.noreply.github.com>
2023-06-30 22:53:15 +0900

    REST API 임시 적용 commit

--- src/main/java/com/example/bpm/controller/UserController.java
+++ src/main/java/com/example/bpm/controller/UserController.java
@@ -14,17 +14,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @Slf4j
 @ToString
 @RequiredArgsConstructor
+@RequestMapping("/user")
 public class UserController {
     @Autowired
     final private UserService userService;
